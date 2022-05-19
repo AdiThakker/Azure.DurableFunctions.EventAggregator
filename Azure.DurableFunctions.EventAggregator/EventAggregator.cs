2a06683348fdb21f2edb67290b7617dfbe0bf58f
Adi Thakker <Aditya.Thakker@gmail.com>
2022-05-19 13:40:35 -0400

    moved usings

--- Azure.DurableFunctions.EventAggregator/EventAggregator.cs
+++ Azure.DurableFunctions.EventAggregator/EventAggregator.cs
@@ -1,11 +1,5 @@
 // Default URL for triggering event grid function in the local environment.
 // http://localhost:7071/runtime/webhooks/EventGrid?functionName=Function1
-using System.Threading.Tasks;
-using Microsoft.Azure.EventGrid.Models;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.DurableTask;
-using Microsoft.Azure.WebJobs.Extensions.EventGrid;
-using Microsoft.Extensions.Logging;
 
 namespace Azure.DurableFunctions.EventAggregator
 {
---
+++ Azure.DurableFunctions.EventAggregator/globalusings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.Azure.WebJobs;
+global using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+global using Microsoft.Extensions.Logging;
+global using System.Threading.Tasks;
+global using Microsoft.Azure.EventGrid.Models;
+global using Microsoft.Azure.WebJobs.Extensions.EventGrid;
